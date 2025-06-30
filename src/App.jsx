9abe283350adebbdcb412ac3a9b66ad44faa2c3a
Om-handa <omhanda246@gmail.com>
2025-06-30 19:52:02 +0530

    Updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import Navbar from './components/Navbar'
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom'
 
 function App() {
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.replace("#", "");
+      const el = document.getElementById(id);
+      if (el) {
+        setTimeout(() => {
+          el.scrollIntoView({ behavior: "smooth" });
+        }, 300); // wait for render
+      }
+    }
+  }, [location]);
 
   return (
     <>
